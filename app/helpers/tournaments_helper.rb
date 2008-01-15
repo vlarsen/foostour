94cb326b7793d4da31d2e4c20524d15e7ccffd67
Vidar Larsen <vlarsen@yahoo-inc.com>
2008-01-15 12:03:43 +0100

    Added use of medal images, and some small visual cleanup of the tournament/show page

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,2 +1,10 @@
 module TournamentsHelper
+  def medal_img(rank)
+    case rank
+    when 1 then image_tag "medals/medal_gold.gif"
+    when 2 then image_tag "medals/medal_silver.gif"
+    when 3 then image_tag "medals/medal_copper.gif"
+    else ""
+    end
+  end
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -13,4 +13,23 @@ class Tournament < ActiveRecord::Base
   def unplayed_matches
     matches.select {|m| m.scores == ""}
   end
+  
+  def self.logo(level)
+    case level
+    when "pro": "tournament/logo_pro_tour.gif"
+    when "master" : "tournament/logo_master_series.gif"
+    when "international": "tournament/logo_international.gif"
+    when "wcs": "tournament/logo_world_championship_series.gif"
+    when "worldchamp": "tournament/logo_world_championships.gif"
+    else "pro"
+    end
+  end
+  
+  def self.structure_descr(structure)
+    case structure
+    when "single": "Single Elimination Cup"
+    when "double": "Double Elimination Cup"
+    else "Unknown structure"
+    end
+  end
 end
