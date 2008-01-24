f292b8a3ac4a5baccdfa26ae0f4959d93ec3026e
Vidar Larsen <vlarsen@yahoo-inc.com>
2008-01-25 00:06:04 +0100

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "foostour"
+set :repository,  "set your repository location here"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+role :app, "core.trondheim.corp.yahoo.com"
+role :web, "core.trondheim.corp.yahoo.com"
+role :db,  "core.trondheim.corp.yahoo.com", :primary => true
